044a045e73360536ecb3321d13c93fff591bbb25
fluodolab3 <fluodolab3@gmail.com>
2022-12-07 15:04:40 +0100

    final

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x9DAb69Ee9c64B4A571D03FFAe2219Ce9303E29D4";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
